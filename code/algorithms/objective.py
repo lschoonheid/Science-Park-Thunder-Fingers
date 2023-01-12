b1ee288a386eaaab2a4c6c51f807382b686624fd
Laszlo Schoonheid <lschoonheid@gmail.com>
2023-01-12 16:53:27 +0100

    skeleton for objective function

--- code/algorithms/objective.py
+++ code/algorithms/objective.py
@@ -1,1 +1,9 @@
+from ..classes.schedule import Schedule
+
 # TODO: #15 Implement objective function which couples a score to a schedule
+def objective(schedule: Schedule) -> float:
+    # TODO: first check whether schedule is at all valid (no doubly booked rooms etc.)
+
+    score: float = 0
+
+    return score
