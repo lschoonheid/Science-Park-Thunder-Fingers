f57af39fe73deeed439d51e941be138ba993c846
Laszlo Schoonheid <lschoonheid@gmail.com>
2023-01-13 14:19:10 +0100

    add requirements file

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+"""
+Interface for a single simulation of boids.
+
+Execute: `python3 main.py -h` for usage.
+
+Student: Laszlo Schoonheid
+Student ID: 11642610
+Course: Programmeerproject 2022
+"""
+
+
+import argparse
+
+
 # TODO: write interface code to execute complete program from command line
+def main ():
+    pass
+
+
+if __name__ == "__main__":
+    # Create a command line argument parser
+    parser = argparse.ArgumentParser(description='Simulate boids flying')
