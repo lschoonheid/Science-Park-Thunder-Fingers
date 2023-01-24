997d5442a6878cbca36e0a660758a88ce80082fe
Laszlo Schoonheid <lschoonheid@gmail.com>
2023-01-24 17:13:27 +0100

    Show gap buckets seperately

--- program_code/visualisation/visualize.py
+++ program_code/visualisation/visualize.py
@@ -12,26 +12,36 @@ from ..classes.result import Result
 def plot_statistics(results: list[Result]):
     evening_timeslots = [result.cached_score_vector[0] for result in results]
     student_overbookings = [result.cached_score_vector[1] for result in results]
-    gaps = [
-        result.cached_score_vector[2] + result.cached_score_vector[3] + result.cached_score_vector[4]
-        for result in results
-    ]
+    gaps_1, gaps_2, gaps_3 = [[result.cached_score_vector[i] for result in results] for i in range(1, 4)]
     total_scores = [result.cached_score for result in results]
 
-    fig, (ax1, ax2, ax3, ax4) = plt.subplots(1, 4, figsize=(9, 4.5), tight_layout=True)
-    ax1.hist(total_scores, 100)
-    ax1.set_title(f"Total score \n mean: ${np.mean(total_scores):.2f} ± {np.std(total_scores):.2f}$")
+    fig, ax = plt.subplots(2, 3, figsize=(9, 4.5), tight_layout=True, sharex=True, sharey=False)
 
-    ax2.hist(evening_timeslots, 100)
-    ax2.set_title(f"Evening timeslots \n mean: ${np.mean(evening_timeslots):.2f} ± {np.std(evening_timeslots):.2f}$")
+    axtot = ax[0, 0]  # type: ignore
+    axtot.hist(total_scores, 100)
+    axtot.set_title(f"Total score \n mean: ${np.mean(total_scores):.2f} ± {np.std(total_scores):.2f}$")
 
-    ax3.hist(student_overbookings, 100)
-    ax3.set_title(
+    axevn = ax[0, 1]  # type: ignore
+    axevn.hist(evening_timeslots, 100)
+    axevn.set_title(f"Evening timeslots \n mean: ${np.mean(evening_timeslots):.2f} ± {np.std(evening_timeslots):.2f}$")
+
+    axconf = ax[0, 2]  # type: ignore
+    axconf.hist(student_overbookings, 100)
+    axconf.set_title(
         f"Course conflicts \n mean: ${np.mean(student_overbookings):.2f} ± {np.std(student_overbookings):.2f}$"
     )
 
-    ax4.hist(gaps, 100)
-    ax4.set_title(f"Gaps \n mean: ${np.mean(gaps):.2f} ± {np.std(gaps):.2f}$")
+    axg1 = ax[1, 0]  # type: ignore
+    axg1.hist(gaps_1, 100)
+    axg1.set_title(f"Gaps \n mean: ${np.mean(gaps_1):.2f} ± {np.std(gaps_1):.2f}$")
+
+    axg1 = ax[1, 1]  # type: ignore
+    axg1.hist(gaps_2, 100)
+    axg1.set_title(f"Gaps \n mean: ${np.mean(gaps_2):.2f} ± {np.std(gaps_2):.2f}$")
+
+    axg1 = ax[1, 2]  # type: ignore
+    axg1.hist(gaps_3, 100)
+    axg1.set_title(f"Gaps \n mean: ${np.mean(gaps_3):.2f} ± {np.std(gaps_3):.2f}$")
 
     plt.savefig("output/image.png")
     plt.show()
