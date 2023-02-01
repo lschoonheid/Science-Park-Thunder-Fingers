486b28ae37428343719755df2d05aaf997802f3c
juliajirina <julianageisler@gmail.com>
2023-02-01 17:30:24 +0100

    cleanup code

--- sched_csv_output.py
+++ sched_csv_output.py
@@ -1,15 +1,15 @@
-"""Output for schedule in .csv file"""
+"""
+Program to convert a schedule object into a csv file.
+Used in main.py
+"""
 
 import csv
-from program_code import Data, Schedule, prepare_path
-
+from program_code import Schedule, prepare_path
 
 def schedule_to_csv(schedule: Schedule, output_path: str = "output/Schedule_output.csv"):
     """Interface for executing scheduling program."""
-    # link random course to random room
-    # schedule.connect_nodes(random.choice(list(schedule.courses)), random.choice(list(schedule.rooms)))
 
-    # 'dag', 'tijdslot' add when implemented elsewhere
+    # columns
     field_names = ["student", "vak", "activiteit", "zaal", "dag", "tijdslot"]
 
     prepare_path(output_path)
@@ -35,19 +35,3 @@ def schedule_to_csv(schedule: Schedule, output_path: str = "output/Schedule_outp
                     writer.writerow(data)
 
         print(f"output saved to {output_path}")
-
-
-if __name__ == "__main__":
-    stud_prefs_path: str = "data/studenten_en_vakken_subset.csv"
-    courses_path: str = "data/vakken_subset.csv"
-    rooms_path: str = "data/zalen.csv"
-
-    input_data = Data(stud_prefs_path, courses_path, rooms_path)
-
-    students_input = input_data.students
-    courses_input = input_data.courses
-    rooms_input = input_data.rooms
-
-    schedule = Schedule(students_input, courses_input, rooms_input)
-
-    schedule_to_csv(schedule)
